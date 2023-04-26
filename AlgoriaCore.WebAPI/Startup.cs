263b294ff8b7054c0324219761cb46e148d3a113
Fernando Castro <fcastro.algoria@outlook.com>
2023-04-26 10:43:07 -0700

    Etiquetas Google Maps

--- AlgoriaCore.WebAPI/Startup.cs
+++ AlgoriaCore.WebAPI/Startup.cs
@@ -261,7 +261,7 @@ namespace AlgoriaCore.WebUI
             {
                 document.PostProcess = v1 =>
                 {
-                    v1.Info.Title = "Algoria Core 230418";
+                    v1.Info.Title = "Algoria Core 230426";
                     v1.Info.Version = "1.0.0";
                     v1.Info.Description = "DatabaseType: " + (appSettings.DatabaseType == DatabaseType.MySql ? "MySql" : "SQL Server");
                 };
